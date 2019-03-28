d324e9865e66be3d16ac96facef4e6a26e0fc914
tkottke90 <altotom90@gmail.com>
2019-03-28 09:51:18 -0500

    feat: Add base to work module

--- src/app/work/work.module.ts
+++ src/app/work/work.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BaseComponent } from './base/base.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BaseComponent],
   imports: [
     CommonModule
   ]
