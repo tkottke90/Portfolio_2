264cba13599998be7c6f4b022dcf3afc477dae7f
tkottke90 <altotom90@gmail.com>
2019-03-28 11:05:53 -0500

    feat: Add Font/Grid Styling to Header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from './header.component';
 import { MatIconModule, MatButtonModule, MatMenuModule } from '@angular/material';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [ HeaderComponent ],
-    imports: [ MatIconModule, MatButtonModule, MatMenuModule, CommonModule, HttpClientModule ],
+    imports: [ MatIconModule, MatButtonModule, MatMenuModule, CommonModule, HttpClientModule, RouterModule ],
     exports: [ HeaderComponent ]
 })
 export class HeaderModule { }
