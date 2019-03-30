eebaef3210a144595bcbf7ab0d120e17cd2af80a
tkottke90 <altotom90@gmail.com>
2019-03-29 22:57:44 -0500

    feat: Add RouterModule to AboutModule

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 import { BaseComponent } from './base/base.component';
 import { AboutRoutingModule } from './about-routing.module';
 import { HeaderModule } from '../header/header.module';
 import { TreeImageComponent } from './tree-image/tree-image.component';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -13,8 +14,10 @@ import { TreeImageComponent } from './tree-image/tree-image.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     AboutRoutingModule,
-    HeaderModule
+    HeaderModule,
+    MatButtonModule
   ]
 })
 export class AboutModule { }
