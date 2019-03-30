035ecb6310912bad90e036ef6783250315859813
tkottke90 <altotom90@gmail.com>
2019-03-29 19:52:37 -0500

    feat: Fix Tree Logic

--- src/app/about/tree-image/tree-image.component.ts
+++ src/app/about/tree-image/tree-image.component.ts
@@ -28,20 +28,24 @@ export class TreeImageComponent implements OnInit {
 
   getDates() {
     const now = new Date();
-    this.sdates.push(new Date(now.getFullYear(), 3, 20));
-    this.sdates.push(new Date(now.getFullYear(), 6, 21));
-    this.sdates.push(new Date(now.getFullYear(), 9, 22));
-    this.sdates.push(new Date(now.getFullYear(), 12, 21));
+    this.sdates.push(new Date(now.getFullYear(), 2, 20));
+    this.sdates.push(new Date(now.getFullYear(), 5, 21));
+    this.sdates.push(new Date(now.getFullYear(), 8, 22));
+    this.sdates.push(new Date(now.getFullYear(), 11, 21));
   }
 
   setSeason() {
     const now = new Date();
-    for (let i = 3; i > 0; i--) {
+    const temp = [];
+    for (let i = 3; i >= 0; i--) {
+      temp.push({ current: now, season: this.sdates[i], result: (now > this.sdates[i])});
       if (now > this.sdates[i]) {
         this.season = i;
         break;
       }
     }
+
+    console.table(temp);
 
     if (this.season === 3) {
       this.festiveLights();
